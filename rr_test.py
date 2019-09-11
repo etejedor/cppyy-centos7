ab915a425e10353453b6a57e65d570651c5f4dee
Enric Tejedor Saavedra <enric.tejedor.saavedra@cern.ch>
2019-09-11 17:20:30 +0200

    Add prints to destructors

--- rr_test.py
+++ rr_test.py
@@ -3,11 +3,16 @@ import cppyy
 cppyy.add_include_path("/tmp/CPyCppyy/src/")
 
 cppyy.cppdef("""
+#include <iostream>
 #include "Python.h"
 #include "Cppyy.h"
 #include "MemoryRegulator.h"
 
 class B : public TObject {
+public:
+   ~B() {
+      std::cout << "B destructor" << std::endl;
+   }
 };
 
 class A : public TObject {
@@ -17,6 +22,7 @@ public:
    void setB(B* bp) { b = bp; }
 
    ~A() {
+      std::cout << "A destructor" << std::endl;
       CPyCppyy::MemoryRegulator::RecursiveRemove(b, Cppyy::GetScope("B"));
       delete b;
    }
@@ -33,4 +39,5 @@ def create():
 
 f, t = create()
 
-print("END OF APP --------------------") 
+print("END OF APP --------------------")
+
